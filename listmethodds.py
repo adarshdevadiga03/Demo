26278f3f84c781a47e43bb9e9d108ca655594ffb
Adarsh <adarshdevadiga033@gmail.com>
2025-11-15 14:46:06 +0530

    Add files via upload

---
+++ listmethodds.py
@@ -0,0 +1,35 @@
+# methods in list
+
+# indexing
+
+#LISTS ARE MUTABLE
+marks= [99,100,52,68]
+print(marks)
+marks[1]= 55
+print(marks)
+
+#slicing
+
+print(marks[1:3])
+print(max(marks))
+print(min(marks))
+
+marks.append(92)
+print(marks)
+
+marks.sort()
+print(marks)
+
+marks.pop(2)
+print(marks)
+
+marks.remove(99)
+print(marks)
+
+marks.insert(2,99)
+print(marks)
+
+# STRINGS ARE IMMUTABLE
+#name="soumya"
+#name[1]= "ddhudfc"
+#print(name)
